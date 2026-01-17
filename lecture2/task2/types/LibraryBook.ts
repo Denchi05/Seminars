c8c088e6b5e55fa1e818786462c539eef6611c2d
Denchi05 <145546015+Denchi05@users.noreply.github.com>
2026-01-17 13:44:44 +0300

    Add lecture2 task2 (TypeScript)

---
+++ lecture2/task2/types/LibraryBook.ts
@@ -0,0 +1,10 @@
+import type { Book } from "./Book";
+import type { Borrowable } from "./Borrowable";
+
+export class LibraryBook implements Borrowable {
+  constructor(public info: Book) {}
+
+  borrow(userName: string): void {
+    console.log(`Книга "${this.info.title}" выдана пользователю ${userName}`);
+  }
+}
